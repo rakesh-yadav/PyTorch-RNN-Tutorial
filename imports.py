f26318f759d071968bef38a527d37be4eb55a1ed
Rakesh Yadav <yadav.r.k.87@gmail.com>
2023-01-11 20:15:18 -0500

    revert model import

--- imports.py
+++ imports.py
@@ -8,7 +8,6 @@ import seaborn as sns
 sns.set_theme(style="darkgrid")
 
 from Utils import *
-from Model import *
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
